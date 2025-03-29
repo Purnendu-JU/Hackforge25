b9228a7a401dd840a79ff3c66789a3ab6f480502
Rupam <royrupamit02@gmail.com>
2025-03-29 16:21:37 +0530

    The Frontend folder is added

---
+++ Frontend/src/pages/FirstAid.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FirstAid = () => {
+  return (
+    <>
+      <h1>First Aid Services</h1>
+    </>
+  )
+}
+
+export default FirstAid
---
+++ Frontend/src/pages/HospitalLocation.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HospitalLocation = () => {
+  return (
+    <>
+      <h1>Hospital Location Service</h1>
+    </>
+  )
+}
+
+export default HospitalLocation
